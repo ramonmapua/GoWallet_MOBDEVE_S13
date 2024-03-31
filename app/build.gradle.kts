6d532619aaad9a8ce1eff4fb2f49fe07127ae6c2
sammysga <samantha.aniversario@gmail.com>
2024-03-31 13:42:59 +0800

    updated add expense

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,7 +46,7 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
     implementation(platform("com.google.firebase:firebase-bom:32.8.0"))
     implementation("com.google.firebase:firebase-analytics")
     implementation ("com.google.firebase:firebase-firestore")
--- app/src/main/java/com/mobdeve/s13/gowallet/AddExpense.kt
+++ app/src/main/java/com/mobdeve/s13/gowallet/AddExpense.kt
@@ -1,16 +1,23 @@
 package com.mobdeve.s13.gowallet
 
+import android.app.DatePickerDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.DatePicker
+import android.widget.EditText
 import androidx.appcompat.widget.Toolbar
+import java.time.Month
+import java.time.Year
+import java.util.Calendar
 
-class AddExpense : AppCompatActivity() {
+class AddExpense : AppCompatActivity(), DatePickerDialog.OnDateSetListener {
 
     lateinit var addReceipt : Button
     lateinit var pinLocation : Button
     lateinit var addExpenseFinish : Button
+    lateinit var etDate: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +28,11 @@ class AddExpense : AppCompatActivity() {
         addReceipt = findViewById(R.id.addReceipt)
         pinLocation = findViewById(R.id.pinLocation)
         addExpenseFinish = findViewById(R.id.addExpenseFinish)
+        etDate = findViewById(R.id.etDate)
+
+        etDate.setOnClickListener {
+            showDatePicker()
+        }
 
         addReceipt.setOnClickListener {
             // TODO: open camera
@@ -32,5 +44,24 @@ class AddExpense : AppCompatActivity() {
         addExpenseFinish.setOnClickListener {
             // TODO: save data
         }
+    }
+
+    private fun showDatePicker() {
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+        val datePickerDialog = DatePickerDialog(this, this, year, month, day)
+        datePickerDialog.show()
+    }
+
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+        val selectedDate = "${padZero(month + 1)}/${padZero(dayOfMonth)}/$year"
+        etDate.setText(selectedDate)
+    }
+
+    private fun padZero(value: Int): String {
+        return if (value < 10) "0$value" else "$value"
     }
 }
